060607465d96133aa3c70dbf7da38dd7fe9eb24f
Cameron <camjohnson0302@gmail.com>
2014-07-07 00:08:59 -0500

    changes source: to class_name: in models

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :game
   has_many :spaces
-  has_many :wins, source: :games
+  has_many :wins, class_name: :games
 end
