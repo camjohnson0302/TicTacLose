153d6798e5be465b3c0b4a4132769f89446684cc
Cameron <camjohnson0302@gmail.com>
2014-07-14 12:33:08 -0500

    bugfix for rapid-clicking

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  $("td").on("click",function() {
-  	if ($(this).css("background-color") == "rgb(255, 255, 255)")
+  var wait = false
+  $(".board td").on("click",function() {
+  	if ($(this).css("background-color") == "rgb(255, 255, 255)" && wait == false)
     {
       $(this).css("background-color","red");
+      wait = true
   		var index = $(this).data("index");
       console.log(index)
   		$.post('/',{index: index},function(response){
-        console.log(response)
         var index = response.index
   			$("[data-index = "+index+"]").css("background-color","blue");
+        wait = false
           if (response.gameOver == true){
             $('#failure_message').css("visibility","visible");
           }
@@ -16,9 +18,14 @@ $(document).ready(function() {
     };
   });
   $(".you_first").on("click",function() {
+    if (wait == false)
+    {
+      wait = true
     $.post('/',{skip: "true"},function(response){
       var index = response.index
       $("[data-index = "+index+"]").css("background-color","blue");
+      wait = false
     });
+    }
   });
 });
